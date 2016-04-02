9190b3bf713b832f65a05bd8d33312239d3fedc8
王信凱 <jcskyting@gmail.com>
2016-04-02 16:22:51 +0800

    add unify plugiin

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,3 +2,19 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap-sprockets
+
+// unify
+//= js/custom
+//= js/app
+//= js/plugins/fancy-box
+//= js/plugins/owl-carousel
+//= js/plugins/style-switcher
+
+// unify plugins
+//= require back-to-top
+//= require smoothScroll
+//= require backstretch/backstretch-ini
+//= require wow-animations/js/wow.min
+//= require jquery.parallax
+//= require fancybox/source/jquery.fancybox.pack
+//= require owl-carousel/owl-carousel/owl.carousel
