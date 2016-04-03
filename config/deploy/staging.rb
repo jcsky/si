9a0b3ecba1e8339ea14643ba72a93ccbb0697eaa
王信凱 <jcskyting@gmail.com>
2016-04-03 12:17:54 +0800

    staging setting

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,11 +1,8 @@
-# server-based syntax
-# ======================
-# Defines a single server with a list of roles and multiple properties.
-# You can define all roles on a single server, or split them:
+set :stage, :staging
+set :rails_env, 'staging'
+set :branch, "develop"
 
-# server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
-# server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
-server '52.89.27.255', user: 'deploy', roles: %w{app db web}, my_property: :my_value
+server '52.89.27.255', user: 'deploy', roles: %w{app db web}, primary: true
 
 
 
