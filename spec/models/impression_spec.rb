e020d1d6249ea91d5b0ff75115412c3f71ab2777
王信凱 <jcskyting@gmail.com>
2016-04-05 05:21:53 +0800

    annotate

--- spec/models/impression_spec.rb
+++ spec/models/impression_spec.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: impressions
+#
+#  id              :uuid             not null, primary key
+#  reputation      :integer
+#  official_name   :string
+#  name            :string
+#  description     :text
+#  impression_type :string
+#  gender          :string
+#  birthday        :date
+#  infos           :hstore
+#  web_pages       :hstore
+#  user_id         :uuid
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe Impression, type: :model do
