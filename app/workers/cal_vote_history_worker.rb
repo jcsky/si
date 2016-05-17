fbd56ea671eb3ab6a3a50ecf93b4df092d5eb07e
王信凱 <jcskyting@gmail.com>
2016-05-17 18:23:46 +0800

    fix worker

--- app/workers/cal_vote_history_worker.rb
+++ app/workers/cal_vote_history_worker.rb
@@ -8,8 +8,8 @@ class CalVoteHistoryWorker
     Impression.all.each do |impression|
       vh = VoteHistory.find_or_create_by(impression_id: impression.id, date_on: Date.today)
       vh.reputation = impression.reputation
-      vh.upvote_user_ids = impression.voters_who_voted_for.pluck(:id)
-      vh.downvote_user_ids = impression.voters_who_voted_against.pluck(:id)
+      vh.upvote_user_ids = impression.votes.where(vote: true).pluck(:voter_id)
+      vh.downvote_user_ids = impression.votes.where(vote: true).pluck(:voter_id)
       vh.save
     end
   end
