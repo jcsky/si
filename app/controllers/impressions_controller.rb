c680a462b0eb213bad92fc9e18dd3f6990793905
王信凱 <jcskyting@gmail.com>
2016-05-24 17:34:53 +0800

    add meta-tags

--- app/controllers/impressions_controller.rb
+++ app/controllers/impressions_controller.rb
@@ -19,6 +19,7 @@ class ImpressionsController < ApplicationController
   layout "home"
   before_action :set_impression, only: [:show, :like, :unlike]
   before_action :set_nav_bar_search_param, only: [:index, :show]
+  before_action :meta_tags_on_show, only: :show
 
   def index
     @q = Impression.ransack(params[:q])
@@ -26,7 +27,7 @@ class ImpressionsController < ApplicationController
     @impressions.last_page? ? @next_page = nil : @next_page = @impressions.next_page
     @load_params = {q: (params[:q] || {})}.merge(page: @next_page)
     respond_to do |format|
-      format.html
+      format.html { set_meta_tags keywords: @impressions.pluck(:name) }
       format.js
     end
   end
@@ -61,5 +62,11 @@ class ImpressionsController < ApplicationController
 
   def set_impression
     @impression = Impression.find(params[:id])
+  end
+
+  def meta_tags_on_show
+    set_meta_tags title: @impression.name,
+      description: @impression.description,
+      keywords: @impression.politician_jobs.pluck(:title).uniq.push(@impression.name)
   end
 end
